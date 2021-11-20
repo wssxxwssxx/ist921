4ff4f50c5015fb53f1b4aa01e46346fd2190a002
wssxxwssxx <wssxxwssxx@gmail.com>
2021-11-20 19:29:21 +0300

    first commit

---
+++ dd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        //df
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
